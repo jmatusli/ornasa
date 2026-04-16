c0b370de8711d456478655b1fe8dda9a9c7b77a6
jamatusli <jamatusli@gmail.com>
2026-04-16 13:25:37 -0600

    modificacion calculo de saldo actual en ventas, modificando StockItemLog

--- app/Model/StockItemLog.php
+++ app/Model/StockItemLog.php
@@ -26,55 +26,23 @@ class StockItemLog extends AppModel {
   }					
 
 	public function getStockQuantityAtDateForFinishedProduct($productId,$rawMaterialId,$productionResultCodeId,$inventoryDate,$warehouseId=0,$boolReturnQuantityOnCurrentDate=false){
+	
 		$inventoryDatePlusOne=date("Y-m-d",strtotime($inventoryDate."+1 days"));
 		$quantityInStock=0;
-		//echo "retrieving the stock quantity with product id ".$productId.", raw material id ".$rawMaterialId.", production result code ".$productionResultCodeId." y inventorydate ".$inventoryDate."<br/>";
-		$conditions=[
-			'StockItem.product_id'=> $productId,
-			'StockItem.raw_material_id'=> $rawMaterialId,
-			'StockItem.production_result_code_id'=> $productionResultCodeId,
-			'StockItem.stockitem_creation_date <='=> $inventoryDate,
-			'StockItem.stockitem_depletion_date >='=> $inventoryDatePlusOne,
-		];
-		if (!empty($warehouseId)){
-			$conditions['StockItem.warehouse_id']=$warehouseId;
-		}
-    //echo "bool current date is ".$boolReturnQuantityOnCurrentDate."<br/>";
-    if ($boolReturnQuantityOnCurrentDate){
-      $conditions['StockItem.remaining_quantity >']= 0;
-    }
-    //pr($conditions);
-		$stockItemIds=$this->StockItem->find('list',[
-			'fields'=>'StockItem.id',
-			'conditions' => $conditions,
-		]);
-		//pr($stockItemIds);
-		if (!empty($stockItemIds)){
-			foreach($stockItemIds as $id=>$stockitemid){
-        $stockItemLogConditions=['StockItemLog.stockitem_id'=>$stockitemid];
-        if (!$boolReturnQuantityOnCurrentDate){
-          $stockItemLogConditions['StockItemLog.stockitem_date <=']=$inventoryDate;
-        }
-        //pr($stockItemLogConditions);
-				$stockItemLog=$this->find('first',[
-					'fields'=>'StockItemLog.product_quantity',
-					'conditions'=>$stockItemLogConditions,
-					'order'=>'StockItemLog.id DESC',
-				]);	
-				//pr($stockItemLog);
-				if (!empty($stockItemLog)){
-					$quantityInStock+=$stockItemLog['StockItemLog']['product_quantity'];
-				}
-				//echo "quantity in stock is ".$quantityInStock."<br/>";
-			}
-		};								
-		return $quantityInStock;
+		
+			//obtenemos el saldo actual
+		$saldo=  $this->StockItem->getSaldo($productId,$warehouseId,$inventoryDate,$rawMaterialId);
+		
+			
+       $quantityInStock= $saldo['total'];
+	   
+	   return $quantityInStock;
 	}
 	
 	public function getStockQuantityAtDateForProduct($productId,$inventoryDate,$warehouseId=0,$boolReturnQuantityOnCurrentDate=false){
 		$inventoryDatePlusOne=date("Y-m-d",strtotime($inventoryDate."+1 days"));
 		$quantityInStock=0;
-		$conditions=[
+		/* $conditions=[
 			'StockItem.product_id'=> $productId,
 			'StockItem.stockitem_creation_date <='=> $inventoryDate,
 			'StockItem.stockitem_depletion_date >='=> $inventoryDatePlusOne,
@@ -106,7 +74,10 @@ class StockItemLog extends AppModel {
 					$quantityInStock+=$stockItemLog['StockItemLog']['product_quantity'];
 				}
 			}
-		};								
+		};		
+ */
+        $saldo=  $this->StockItem->getSaldo($productId,$warehouseId,$inventoryDate,0);	
+       $quantityInStock= $saldo['total']; 		
 		return $quantityInStock;
 	}
 
